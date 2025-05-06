698ed0b8819c1bb66f48e8ae491b6b5483fc9978
Pedro Luiz <pedrolmm123321@gmail.com>
2025-05-06 10:20:28 -0300

    db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,20 +1,21 @@
-// src/config/db.js
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-// (opcional) desliga warnings de strictQuery
 mongoose.set('strictQuery', false);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: 'UniGym'         
+    });
     console.log('✅ MongoDB conectado com sucesso');
   } catch (err) {
     console.error('❌ Erro ao conectar no MongoDB:', err);
     process.exit(1);
   }
 
-  // eventos (opcional)
   mongoose.connection
     .on('disconnected', () => console.log('❌ Mongoose desconectado'))
     .on('error', err => console.error('❌ Erro Mongoose:', err));
