8b716df361f10ed28e742db123be939a35338673
Pedro Luiz <pedrolmm123321@gmail.com>
2025-05-29 10:10:18 -0300

    first commit 2

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const connectDB = require('./src/config/db');
-const app       = require('./app');
+const app = require('./app');
 
 connectDB();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 UniGym-Backend rodando em http://localhost:${PORT}`);
+  console.log(`UniGym-Backend rodando em http://localhost:${PORT}`);
 });
